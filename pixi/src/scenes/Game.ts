546d58a7836190123128cbf8b9734d854b834c0e
Theta Nil <20186755+thetanil@users.noreply.github.com>
2024-02-17 20:37:57 +0100

    test e

--- pixi/src/scenes/Game.ts
+++ pixi/src/scenes/Game.ts
@@ -1,51 +1,66 @@
 import config from "../config";
+import { Graphics, Text } from "pixi.js";
 import ParallaxBackground from "../prefabs/ParallaxBackground";
 import { Player } from "../prefabs/Player";
 import Scene from "../core/Scene";
 import SpineAnimation from "../core/SpineAnimation";
 
 export default class Game extends Scene {
-  name = "Game";
+    name = "Game";
 
-  private player!: Player;
-  private background!: ParallaxBackground;
+    private player!: Player;
+    private background!: ParallaxBackground;
 
-  load() {
-    this.background = new ParallaxBackground(config.backgrounds.forest);
-    this.player = new Player();
+    load() {
+        this.background = new ParallaxBackground(config.backgrounds.forest);
+        this.player = new Player();
 
-    this.player.x = window.innerWidth / 2;
-    this.player.y = window.innerHeight - this.player.height / 3;
+        this.player.x = window.innerWidth / 2;
+        this.player.y = window.innerHeight - this.player.height / 3;
 
-    this.background.initPlayerMovement(this.player);
+        this.addChild(this.background, this.player);
 
-    this.addChild(this.background, this.player);
-  }
+        let obj = new Graphics();
+        obj.beginFill(0x000000);
+        obj.drawRect(this.player.x, this.player.y, 100, 100);
+        obj.endFill();
+        this.addChild(obj);
 
-  async start() {
-    // Example of how to play a spine animation
-    const vine = new SpineAnimation("vine-pro");
+        const basicText = new Text('Basic text in pixi');
 
-    vine.stateData.setMix("grow", "grow", 0.5);
+        basicText.x = 50;
+        basicText.y = 100;
+        basicText.style.fill = 0xFFFFFF;
+        this.addChild(basicText);
 
-    vine.x = 0;
-    vine.y = window.innerHeight / 2 - 50;
+        // this.background.initPlayerMovement(this.player);
 
-    this.background.addChild(vine);
-
-    while (vine) {
-      await vine.play("grow");
-    }
-  }
-
-  onResize(width: number, height: number) {
-    if (this.player) {
-      this.player.x = width / 2;
-      this.player.y = height - this.player.height / 3;
     }
 
-    if (this.background) {
-      this.background.resize(width, height);
+    async start() {
+        // Example of how to play a spine animation
+        const vine = new SpineAnimation("vine-pro");
+
+        vine.stateData.setMix("grow", "grow", 0.5);
+
+        vine.x = 0;
+        vine.y = window.innerHeight / 2 - 50;
+
+        // this.background.addChild(vine);
+
+        // while (vine) {
+        //   await vine.play("grow");
+        // }
     }
-  }
+
+    onResize(width: number, height: number) {
+        if (this.player) {
+            this.player.x = width / 2;
+            this.player.y = height - this.player.height / 3;
+        }
+
+        if (this.background) {
+            this.background.resize(width, height);
+        }
+    }
 }
